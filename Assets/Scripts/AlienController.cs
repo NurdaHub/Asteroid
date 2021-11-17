d9ce947dfbfbc418cdbceb4cfa6f2a31c11eefa9
NurdaHub <asilbekoff.nurd@gmail.com>
2021-11-18 00:28:40 +0600

    add alien

---
+++ Assets/Scripts/AlienController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AlienController : MonoBehaviour
+{
+    private Rigidbody2D alienRB;
+
+    private void OnEnable()
+    {
+        var alienSpeed = Random.Range(70f, 100f);
+        alienRB = GetComponent<Rigidbody2D>();
+        alienRB.AddForce(transform.right * alienSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid"))
+            gameObject.SetActive(false);
+    }
+}
